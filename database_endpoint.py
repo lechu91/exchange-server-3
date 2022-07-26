a5e43b98ba9ebce70dd62f4af7686ae0e5b92c15
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-07-25 23:52:23 -0700

    Update database_endpoint.py

--- database_endpoint.py
+++ database_endpoint.py
@@ -30,8 +30,9 @@ def shutdown_session(response_or_exc):
 -------- Helper methods (feel free to add your own!) -------
 """
 
-def log_message(d)
+def log_message(d):
     # Takes input dictionary d and writes it to the Log table
+    
     pass
 
 """
